8748dede34a343dbbe366ec8554f4a6920211dbc
octubree <jorgitobaliero@gmail.com>
2023-11-15 10:52:48 -0300

    probando

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,25 @@
 * License: https://bootstrapmade.com/license/
 */
 
+  // OCULTA IMAGEN UTE CUANDO ABRE MENÚ EN MOBILES
+
+  // Get the image element
+  const imageElement = document.querySelector(".mobile-only img");
+  
+  // Get the navigation menu element
+  const navigationMenuElement = document.querySelector(".navbar");
+  
+  // Listen for the click event on the navigation menu
+  navigationMenuElement.addEventListener("click", function() {
+    // Hide the image
+    imageElement.style.display = "none";
+  });
 
 
+
+
+// Hide the image on desktop devices
+hideImageOnDesktop();
 
 // EMPIEZA JAVASCRIPT DE QUE VINO CON LA PLANTILLA POR DEFECTO 
 
